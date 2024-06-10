57dbf0151bc49c2282d63d76536b21014f9a0e39
Priyanha <priya.param6@gmail.com>
2024-06-10 20:10:19 +1000

    first commit

---
+++ backend/config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI: process.env.MONGO_URI || 'mongodb+srv://treetest28:treetest28@cluster0.y4zxtwn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/secure-chat-app',
+  secretOrKey: process.env.JWT_SECRET || 'your_secret_key',
+  emailUser: process.env.EMAIL || 'mynewsocial12345@gmail.com',
+  emailPassword: process.env.EMAIL_PASSWORD || 'vkqo qafe aoqu jrfa',
+};
